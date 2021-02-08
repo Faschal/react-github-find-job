15fd87690a07d315a939d167be3847173f9e70b9
Faschal <faschal21@gmail.com>
2021-02-08 17:06:02 +0700

    Added list job

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+import useFetchJobs from './components/useFetchJobs';
+import { Container } from 'react-bootstrap';
+import Job from './components/Job';
+
 function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const {jobs, loading, error} = useFetchJobs(params, page);
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error, try refresh</h1>}
+      <h1>{jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}</h1>
+    </Container>
   );
 }
 
